a48f935b9898857babdcccde632991b15d775fff
LBrownI <lucasbrowngd@gmail.com>
2025-04-02 00:05:11 -0300

    solve lint

--- app/main.py
+++ app/main.py
@@ -4,14 +4,17 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/time")
 def get_current_time():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return {"current_time": current_time}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
